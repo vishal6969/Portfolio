ac7c710a970f2244027f2cbd61372247376a999e
Vishal <vm925623@gmail.com>
2021-09-21 13:14:32 +0530

    added project gallery

--- script.js
+++ script.js
@@ -16,3 +16,41 @@ function welcome(){
 }
 
 let interval = setInterval(welcome,25);
+
+//project gallery
+var arr = ["https://vishal6969.github.io/Game-Zone",
+"https://vishal6969.github.io/Paint",
+"https://vishal6969.github.io/rgb-game",
+"https://vishal6969.github.io/To-Do-List",
+"https://vishal6969.github.io/canvas_clock"];
+
+var frame = document.querySelector("iframe");
+var index1 = 0;
+
+//function to set url of frames
+function urlSetter(i) {
+    if(i < 0 || i >= arr.length) {
+        return;
+    }
+    frame.src = "";
+    frame.src = arr[i];
+    index1 = i;
+}
+urlSetter(index1);
+
+//functions to manipulate frames
+function left() {
+    urlSetter(index1-1);
+}
+
+function right() {
+    urlSetter(index1+1);
+}
+
+//function to prevent default operations
+document.querySelector("#btn1").addEventListener("mousedown",function(e){
+    e.preventDefault();
+});
+document.querySelector("#btn2").addEventListener("mousedown",function(e){
+    e.preventDefault();
+});
